4557018931fe420e385662453b97254d3d641c40
antaresgeo <ctrujillozt@gmail.com>
2016-10-07 12:53:43 -0500

    correcciones

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='exileui',
-    version='0.1.8',
+    version='0.1.8.2',
     packages=find_packages(),
     include_package_data=True,
     license='GNU GENERAL PUBLIC LICENSE',
