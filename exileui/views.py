1651d471f96116a63eb214078f860dfecae1789f
antaresgeo <ctrujillozt@gmail.com>
2016-12-01 14:01:08 -0500

    correciones de configuracion de colores

--- exileui/views.py
+++ exileui/views.py
@@ -6,13 +6,13 @@ from django.conf import settings
 def colors(request):
     try:
         theme = settings.THEME
-    except e:
+    except:
         theme = False
     # end try
     if theme:
         return render(request, 'exileui/colors.css', {
             'accent': theme.get('accent'),
-            'accent_ligth': theme.get('accent_ligth'),
+            'accent_light': theme.get('accent_light'),
             'primary': theme.get('primary'),
             'secondary': theme.get('secondary')
         }, content_type='text/css')
